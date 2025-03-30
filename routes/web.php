9e1a02e33f5c4dcf690091101dcf3b3fb7086d01
julesroy <112734731+julesroy@users.noreply.github.com>
2025-03-30 23:02:39 +0200

    Ajout de configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/a-propos', function() {
+    return view('a-propos');
 });
