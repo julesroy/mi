f9ba7185324dd4ab65abefb1084f5095024206f4
Théo LESAGE <theo.lesage@student.junia.com>
2025-05-07 22:32:28 +0200

    structure de la page compte + début de la mise en place des boutons "Changer mdp" et "Déconnexion".

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -8,7 +8,33 @@
     <body class="bg-[#0a0a0a] text-white pt-28 md:pt-36">
         @include('header')
 
-        <p>Compte</p>
+        <div class="flex min-h-full flex-lin justify-center px-6 py-6 lg:px-8"> <!-- div main compte -->
+
+            <div> <!-- Données de compte -->
+            </div>
+
+
+            <div> <!-- liste des commandes actuelles -->
+            </div>
+
+
+            <div> <!-- Changer mot de passe ? & Déconnexion-->
+                
+                <div class="flex justify-center min-h-20 min-w-80"><!-- Changer mot de passe ? -->
+                    <button class="bg-gray-700 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer m-2 min-w-full min-h-full"> 
+                        Changer votre mot de passe 
+                    </button>
+                </div>
+
+                <div class="flex justify-center min-h-20 min-w-80"> <!-- Déconnexion -->
+                    <button class="bg-gray-700 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer m-2 min-w-full min-h-full">
+                        Déconnexion
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
 
         @include('footer')
     </body>
