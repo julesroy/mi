a7164006d49ce9a5ba0ada69cbad351240625ecd
Théo LESAGE <theo.lesage@student.junia.com>
2025-05-08 11:36:48 +0200

    structure /compte completé (sans traitement données)

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -8,26 +8,63 @@
     <body class="bg-[#0a0a0a] text-white pt-28 md:pt-36">
         @include('header')
 
-        <div class="flex min-h-full flex-lin justify-center px-6 py-6 lg:px-8"> <!-- div main compte -->
+        <div class="flex place-self-center relative min-h-full flex-col justify-center px-6 py-6"> <!-- div main compte -->
 
-            <div> <!-- Données de compte -->
-            </div>
+            <div class="flex flex-col border-3 border-y-gray-600 mb-8"> <!-- liste des commandes actuelles -->
+                <div class="bg-gray-900">    
+                    <p class="text-2xl place-self-center p-1"> Vos commandes </p>
+                </div>
 
-
-            <div> <!-- liste des commandes actuelles -->
-            </div>
-
-
-            <div> <!-- Changer mot de passe ? & Déconnexion-->
+                <div class="flex flex-col bg-gray-800 p-2">
+                    <p> ... </p>
+                    <p> ... </p>
+                </div>
                 
-                <div class="flex justify-center min-h-20 min-w-80"><!-- Changer mot de passe ? -->
-                    <button class="bg-gray-700 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer m-2 min-w-full min-h-full"> 
+            </div>
+
+
+            <div class="flex flex-col border-3 border-y-gray-600"> <!-- Données de compte -->
+
+                <div class="bg-gray-900"> <!-- Titre -->  
+                    <p class="text-2xl place-self-center p-1"> Vos données </p>
+                </div>
+
+                <div class="flex flex-col bg-gray-800 p-2"> <!-- Données -->  
+                    <div class="flex flex-row"> <!-- Nom -->
+                        <p class="font-bold pr-3"> Nom : </p>
+                        <p> ... </p>  
+                    </div>
+
+                    <div class="flex flex-row"> <!-- Prenom -->
+                        <p class="font-bold pr-3"> Prenom : </p>
+                        <p> ... </p>  
+                    </div>
+
+                    <div class="flex flex-row"> <!-- Adresse mail -->
+                        <p class="font-bold pr-3"> Adresse mail : </p>
+                        <p> ... </p>  
+                    </div>
+
+                    <div class="flex flex-row"> <!-- Numéro compte (identifiant) -->
+                        <p class="font-bold pr-3"> Identifiant : </p>
+                        <p> ... </p>  
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="flex flex-col justify-center min-h-40 min-w-80"> <!-- Changer mot de passe ? & Déconnexion-->
+                
+                <div ><!-- Changer mot de passe ? -->
+                    <button class="bg-gray-800 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer mb-1 min-w-full min-h-full"> 
                         Changer votre mot de passe 
                     </button>
                 </div>
 
-                <div class="flex justify-center min-h-20 min-w-80"> <!-- Déconnexion -->
-                    <button class="bg-gray-700 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer m-2 min-w-full min-h-full">
+                <div > <!-- Déconnexion -->
+                    <button class="bg-gray-800 px-5 py-2 hover:bg-gray-400 hover:cursor-pointer mt-1 min-w-full min-h-full">
                         Déconnexion
                     </button>
                 </div>
